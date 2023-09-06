3e251cbea07c9f288f485082cf6a022b7354f7b2
nassef gharsalli <nassefg216@gmail.com>
2023-09-06 12:44:55 +0100

    first commit

---
+++ src/Player.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={imageUrl} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          <p>Team: {team}</p>
+          <p>Nationality: {nationality}</p>
+          <p>Jersey Number: {jerseyNumber}</p>
+          <p>Age: {age}</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Player.defaultProps = {
+  name: "Unknown",
+  team: "Unknown",
+  nationality: "Unknown",
+  jerseyNumber: "Unknown",
+  age: "Unknown",
+  imageUrl: "https://t4.ftcdn.net/jpg/01/05/25/85/360_F_105258508_OkmBS2R5Eld0Zs3wRJBvAzLfTllxQGQe.jpg",
+};
+
+export default Player;
---
+++ src/players.js
@@ -0,0 +1,37 @@
+
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentine",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://images.mlssoccer.com/image/private/t_editorial_landscape_8_desktop_mobile/mls-mia-prd/xyfcjysnblxkkprtwect.png",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 38,
+      imageUrl: "https://dailypost.ng/wp-content/uploads/2022/12/Collage-Maker-31-Dec-2022-07.53-AM.jpg.webp",
+    },
+    {
+      name: "Neymar Jr.",
+      team: "Al-Hilal",
+      nationality: "Brazilian",
+      jerseyNumber: 10,
+      age: 31,
+      imageUrl: "https://www.h24info.ma/wp-content/uploads/2023/08/Neymar-vers-Al-Hilal-1024x538.jpg",
+    },
+    {
+      name: "Jude Bellingham",
+      team: "Real Madrid",
+      nationality: "England",
+      jerseyNumber: 5,
+      age: 20,
+      imageUrl: "https://pbs.twimg.com/media/FvNlFnIWYAAk5QQ.jpg:large",
+    },
+  ];
+  
+  export default players;
